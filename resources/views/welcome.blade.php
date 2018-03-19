d0c67c40e0600b144a9b28a228e7ac32551948e8
Josue Plaza <plazajosue2@gmail.com>
2018-03-18 20:09:16 -0500

    first blood

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Limeade Labs | Home</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <link href="/css/app.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <h1>Hello world</h1>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
